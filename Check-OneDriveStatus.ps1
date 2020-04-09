f82d3389069837c59b9cd2cd3b229c41cd41d293
Perry Stathopoulos <Perry@crestline.net>
2020-04-09 13:51:06 -0400

    improved checking logic

--- Check-OneDriveStatus.ps1
+++ Check-OneDriveStatus.ps1
@@ -9,17 +9,34 @@ $ODErrorCount = 0
 
 $statusFiles = Get-ChildItem -Path $odStatusFiles
 
-foreach ($odStatusFile in $statusFiles){
-
-	$ODStatus = (get-content $odStatusFile | convertfrom-json).value
-
-	foreach ($ODStat in $ODStatus) {
-		if ($ODStat.StatusString -in $ErrorList) { 
-			$ODOutputStatus = "$($ODStat.LocalPath) is in state $($ODStat.StatusString)" 
-			$ODErrorCount++
+if ($statusFiles.Count -gt 0){
+    #we have status files but check that at least one is at least 2 hours old
+    $hasRecentUpdate = $false
+    foreach ($odStatusFile in $statusFiles){
+        $deltaTime = Get-ChildItem $odStatusFile | New-TimeSpan
+        if (!($hasRecentUpdate) -and ($deltaTime.TotalHours -lt 2)) {
+            $hasRecentUpdate = $true
+        }
+    }
+	
+	if ($hasRecentUpdate) {
+		foreach ($odStatusFile in $statusFiles){
+			$ODStatus = (get-content $odStatusFile | convertfrom-json).value
+			foreach ($ODStat in $ODStatus) {
+				if ($ODStat.StatusString -in $ErrorList) { 
+					$ODOutputStatus = "$($ODStat.LocalPath) is in state $($ODStat.StatusString)" 
+					$ODErrorCount++
+				}
+			}
 		}
+	} else {
+		$ODOutputStatus = "No recent updates for OneDrive Status files in at least 2 hours" 
+		$ODErrorCount = -2	
 	}
+} else {
+	$ODOutputStatus = "No status files" 
+	$ODErrorCount = -1
 }
 # test output
-#Write-Host $ODOutputStatus
-#Write-Host $ODErrorCount
+Write-Host $ODOutputStatus
+Write-Host $ODErrorCount
